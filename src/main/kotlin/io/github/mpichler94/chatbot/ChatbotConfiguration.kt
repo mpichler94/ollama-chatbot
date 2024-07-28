272dea14fc58a4a9514579d5681fdd53a34faed1
Marcel Pichler <marcel.pichler@outlook.at>
2024-07-28 12:44:29 +0200

    feat: add a token splitter to improve the embedding quality

--- src/main/kotlin/io/github/mpichler94/chatbot/ChatbotConfiguration.kt
+++ src/main/kotlin/io/github/mpichler94/chatbot/ChatbotConfiguration.kt
@@ -1,10 +1,12 @@
 package io.github.mpichler94.chatbot
 
+import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
 import org.springframework.ai.reader.ExtractedTextFormatter
 import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader
 import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig
+import org.springframework.ai.transformer.splitter.TokenTextSplitter
 import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.boot.ApplicationRunner
@@ -14,6 +16,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ChatbotConfiguration {
+    private val logger = LoggerFactory.getLogger(ChatbotConfiguration::class.java)
 
 //    @Bean
     fun initVectorStore(vs: VectorStore): ApplicationRunner {
@@ -32,23 +35,24 @@ class ChatbotConfiguration {
                 .build()
 
 
+            val tokenSplitter = TokenTextSplitter()
             val pdfReader1 = ParagraphPdfDocumentReader("file:T:/IEC 62443-4-1 (2018-01).pdf", config)
-            vs.add(pdfReader1.read())
+            vs.add(tokenSplitter.apply(pdfReader1.read()))
             val pdfReader2 = ParagraphPdfDocumentReader("file:T:/IEC 62443-4-2 (2019-02).pdf", config)
-            vs.add(pdfReader2.read())
+            vs.add(tokenSplitter.apply(pdfReader2.read()))
             val pdfReader3 = ParagraphPdfDocumentReader("file:T:/IEC 62443-2-3 (2015-06).pdf", config)
-            vs.add(pdfReader3.get())
+            vs.add(tokenSplitter.apply(pdfReader3.get()))
             val pdfReader4 = ParagraphPdfDocumentReader("file:T:/IEC 62443-3-3 (2013-08).pdf", config)
-            vs.add(pdfReader4.get())
+            vs.add(tokenSplitter.apply(pdfReader4.get()))
+            logger.info("All documents added")
         }
     }
 
     @Bean
-//    @DependsOn("initVectorStore")
     fun userPrompt(chatClient: ChatClient): ApplicationRunner {
         return ApplicationRunner {
             val prompt = """
-                    What do I need to keep in mind regarding patching?
+                    What do I need to know regarding patching?
                     
 					""".trimIndent()
             System.out.println("Q: $prompt")
