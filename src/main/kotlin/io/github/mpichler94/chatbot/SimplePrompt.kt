04abe1693d08a0084a7ba5dc2c5f9c5a3a539c49
Marcel Pichler <marcel.pichler@outlook.at>
2025-05-03 18:44:55 +0200

    feat: add memory to the simple prompt

--- src/main/kotlin/io/github/mpichler94/chatbot/SimplePrompt.kt
+++ src/main/kotlin/io/github/mpichler94/chatbot/SimplePrompt.kt
@@ -1,19 +1,24 @@
 package io.github.mpichler94.chatbot
 
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
 import org.springframework.ai.chat.client.entity
 import org.springframework.ai.chat.memory.InMemoryChatMemory
 import org.springframework.ai.chat.prompt.PromptTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/persons")
-class PersonController(chatClientBuilder: ChatClient.Builder) {
-    val chatClient: ChatClient = chatClientBuilder.build()
+class PersonController(chatClientBuilder: ChatClient.Builder, chatMemory: InMemoryChatMemory) {
+    val chatClient: ChatClient = chatClientBuilder
+        .defaultAdvisors(PromptChatMemoryAdvisor(chatMemory), SimpleLoggerAdvisor())
+        .build()
 
     @GetMapping
     fun findAll(): List<Person> {
@@ -26,6 +31,20 @@ class PersonController(chatClientBuilder: ChatClient.Builder) {
         return chatClient.prompt(template.create())
             .call()
             .entity<List<Person>>()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): Person {
+        val template = PromptTemplate("""
+            Find and return the object with id {id} in a current list of persons.
+            
+        """.trimIndent())
+
+            val p = template.create(mapOf("id" to id))
+
+        return chatClient.prompt(p)
+            .call()
+            .entity()
     }
 
 }
@@ -43,10 +62,10 @@ enum class Gender {
     MALE, FEMALE
 }
 
-//@Configuration
-//class SimplePromptConfiguration {
-//    @Bean
-//    fun chatMemory(): InMemoryChatMemory {
-//        return InMemoryChatMemory()
-//    }
-//}
+@Configuration
+class SimplePromptConfiguration {
+    @Bean
+    fun chatMemory(): InMemoryChatMemory {
+        return InMemoryChatMemory()
+    }
+}
